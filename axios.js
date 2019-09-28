f2b2299af3eab13ce12f150f97bc8ad60570810b
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-27 23:26:36 -0400

    Initial commit

---
+++ axios.js
@@ -0,0 +1,21 @@
+let body = document.body
+
+
+let ul = document.createElement('ul');
+let li = document.createElement('li');
+
+
+axios.get("https://sei-api.herokuapp.com/students").then((response) => {
+    let data = response.data;
+    data.forEach(element => {
+        let nameContainer = document.createElement('strong');
+        nameContainer.innerText = capitalize(element.name);
+        let knownForContainer = document.createElement('em');
+        knownForContainer.innerText = element['known-for']
+        body.append(nameContainer, knownForContainer);
+    });
+})
+
+let capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
