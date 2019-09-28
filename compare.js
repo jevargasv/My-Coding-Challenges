a82a60e81d1b0fedc1d29be2673d028f5b1768dd
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-27 23:17:43 -0400

    Initial commit

---
+++ compare.js
@@ -0,0 +1,20 @@
+let compare = (numbers, value) => {
+    let lessThan = [];
+    let greaterThan = [];
+    let equalTo = [];
+    for (i = 0; i < numbers.length; i++) {
+        if (numbers[i] < value) 
+            lessThan.push(numbers[i]);
+        else if (numbers[i] > value)
+            greaterThan.push(numbers[i]);
+        else if (numbers[i] = value)
+            equalTo.push(numbers[i]);
+        else
+            return "There is nothing to compare, you dingbats!";
+      
+    }
+  
+
+}
+
+compare([10, 30, 45, 4, 21, 30], 30)
