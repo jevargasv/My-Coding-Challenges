2147ed6fc351f68d268b4b00726bafd1353d6821
Jennifer Vargas <jevargasv1990@gmail.com>
2019-10-01 21:01:10 -0400

    Initial commit

---
+++ point.js
@@ -0,0 +1,123 @@
+// Calculator Function
+
+let calculator = (answer) => {
+    if(answer === 6) {
+        return 4 + 2 // 6
+    } else if(answer === 2) {
+        return 4 - 2 // 2
+    } else if(answer === 2) {
+        return 4 / 2 // 2
+    } else(answer === 8) {
+        return 4 * 2 // 8
+    }
+}
+
+console.log(answer) // 6
+function calculator(4, 2) {
+    return 4 - 2 // 2
+}
+answer = calculator("subtract", 4, 2)
+console.log(answer) // 2
+function calculator(4, 2) {
+    return 4 / 2 // 2
+}
+answer = calculator("divide", 4, 2)
+console.log(answer) // 2
+function calculator(4, 2) {
+    return 4 * 2 // 8
+}
+answer = calculator("multiply", 4, 2)
+console.log(answer) // 8
+
+// Kardashian Quiz
+
+let ask = (point) => {
+    if(point === true) {
+        return "yes" // true
+    } else {
+        return "no" // false
+    }
+}
+
+let setofQuestions = (point) => {
+    return ask(point)
+
+}
+let point = ask("Is Kim the best?", "true")
+console.log(point)
+
+function setofQuestions(question, answer) {
+    return ["Was Kim born in 1985?", "Will Rob get out of jail?", "Does North seem happy?", "Kanye released 'Heartless' in 2008?"] // [yes, no, no, yes]
+}
+
+let questions = [
+    "Was Kim born in 1985?",
+    "Will Rob get out of jail?",
+    "Does North seem happy?",
+    "Kanye released 'Heartless' in 2008?"
+]
+
+let answers = [
+    "yes",
+    "no",
+    "no",
+    "yes"
+]
+
+// FUCK THE KARDASHIANS, MEET THE WEASLEYS!!
+
+let questionsone = [
+    "Do the Weasleys have a daughter?",
+    "Is Fred Weasley dead?",
+    "Is Arthur Weasley an Auror?",
+    "Are the Weasleys Muggles?",
+    "Do they all have red hair and freckles?"
+    "Do they still have their Ford Anglia?"
+
+]
+
+let answersone = [
+    "yes",
+    "yes",
+    "no",
+    "no",
+    "yes",
+    "no"
+]
+
+let questionstwo = [
+    "What is the daughter's name?",
+    "What date did he die?",
+    "What is his occupation?",
+    "What is their magic background?",
+    "Where did they get it from?",
+    "What car do they now have?"
+]
+
+let answerstwo = [
+    "Ginny Weasley",
+    "1997, R.I.P.",
+    "Muggle Artifact Collector",
+    "Pure-bloods",
+    "From their grandfather",
+    "Jaguar"
+]
+
+let ask = (point) => {
+    if(point === true) {
+        return "yes" // true
+    } else {
+        return "no" // false
+    }
+}
+
+let setofQuestionsOne = (point) => {
+    return ask(point)
+
+}
+let point = ask("Is Fred Weasley dead?, "true");
+console.log(point)
+
+function setofQuestions(question, answer) {
+    return ["Was Kim born in 1985?", "Will Rob get out of jail?", "Does North seem happy?", "Kanye released 'Heartless' in 2008?"] // [yes, no, no, yes]
+}
