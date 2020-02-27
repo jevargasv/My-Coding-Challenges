ccf922667d7b201a58ecd8fb25a9c246ee413329
Jennifer Vargas <jevargasv1990@gmail.com>
2020-02-26 21:21:13 -0500

    Initial commit

--- oop_challenges.js
+++ oop_challenges.js
@@ -5,17 +5,17 @@ let car = {
     brand: 'furd',
     name: 'daisy',
     drive: function() {
-      console.log('vroom vroom')
+      console.log('vroom vroom');
     }
     getBrand: function() {
-      console.log(this.brand)
+      console.log(this.brand);
     }
   }
   
   
   class Animal {
-    constructor(name){
-      this.name = name
+    constructor(name) {
+      this.name = name;
     }
   
     speak() {
@@ -30,7 +30,7 @@ let car = {
   }
   
   let buddy = new Dog();
-  buddy.speak()
+  buddy.speak();
   
   //Example One 
   
@@ -38,32 +38,32 @@ let car = {
   let multiplier = {
     value: 1,
     multiply: function(multiplier) {
-      this.value = this.value * multiplier
+      this.value = this.value * multiplier;
     },
     getCurrentValue: function() {
-      return this.value
+      return this.value;
     }
   }
   
-  multiplier.getCurrentValue()// Returns 1 if you have not run any code on it
-  multiplier.multiply(25)
-  multiplier.getCurrentValue() //Returns 25
-  multiplier.multiply(2)
-  multiplier.getCurrentValue() //Returns 50
+  multiplier.getCurrentValue(); // Returns 1 if you have not run any code on it
+  multiplier.multiply(25);
+  multiplier.getCurrentValue(); //Returns 25
+  multiplier.multiply(2);
+  multiplier.getCurrentValue(); //Returns 50
   
   
   //Example Two - Album
   
     class Album{
-      constructor(name){
-        this.name = name
-        this.photos = []
+      constructor(name) {
+        this.name = name;
+        this.photos = [];
       }
   
-      addPhoto(photoInput){
+      addPhoto(photoInput) {
         photoInput.id = this.photos.length;
-        console.log(photoInput)
-        this.photos.push(photoInput)
+        console.log(photoInput);
+        this.photos.push(photoInput);
       }
   
       findPhoto(locationMatched) {
@@ -72,42 +72,42 @@ let car = {
   
       listPhotos() {
         for(photo of this.photos) {
-          console.log(photo)
+          console.log(photo);
         }
       }
     }
   
-    class Photo{
-      constructor(link,place,id){
-        this.link = link
-        this.place = place
+    class Photo {
+      constructor(link,place,id) {
+        this.link = link;
+        this.place = place;
       }
     }
   
   
-     let familyAlbum = new Album('Family Vacation #1')
-     let famphoto = new Photo('http://i.imgur.com/07OiOrs.png', 'Italy')
-     let famphototwo = new Photo('http://Blah.png', 'Spain')
-     let famphotothree = new Photo('jshdashdjsd', 'Italy')
+     let familyAlbum = new Album('Family Vacation #1');
+     let famphoto = new Photo('http://i.imgur.com/07OiOrs.png', 'Italy');
+     let famphototwo = new Photo('http://Blah.png', 'Spain');
+     let famphotothree = new Photo('jshdashdjsd', 'Italy');
       //Each method may or may not need arguments. The dots dont represent anything.
-      familyAlbum.addPhoto(famphoto)
-      familyAlbum.addPhoto(famphototwo)
-      familyAlbum.addPhoto(famphotothree)
+      familyAlbum.addPhoto(famphoto);
+      familyAlbum.addPhoto(famphototwo);
+      familyAlbum.addPhoto(famphotothree);
       // familyAlbum.listPhotos()
       // familyAlbum.findPhoto('Italy')
   
   
   //Family - this isnt the ideal scenerio and would only be relevant if you were doing one family
   
-  class Grandfather{
-    constructor(name,goodGenetics){
-      this.name = name
-      this.goodGenetics = goodGenetics
+  class Grandfather {
+    constructor(name,goodGenetics) {
+      this.name = name;
+      this.goodGenetics = goodGenetics;
     }
   }
   
   class Father extends Grandfather {
-    constructor(familyName, age){
+    constructor(familyName, age) {
       super('Bob', true);
       this.familyName = familyName;
       this.age = age;
@@ -116,7 +116,7 @@ let car = {
   
   class Son extends Father { 
     constructor(firstName, goodChild) {
-      super('Patlut', 50)
+      super('Patlut', 50);
       this.firstName = firstName;
       this.goodChild = goodChild;
     }
