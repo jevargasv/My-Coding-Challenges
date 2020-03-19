6c90895fed9d437f8afcbc9bfb4a230ae3096434
Jennifer Vargas <jevargasv1990@gmail.com>
2020-03-18 23:30:04 -0400

    Initial commit

--- dom.js
+++ dom.js
@@ -74,6 +74,7 @@ document.getElementById('myId').innerText = "This is my new header two.";
 
 document.getElementById('container').innerText;
 
+// Add event listener
 
 let submitButton = document.getElementbyId("submit-button");
 let inputBox = document.getElementbyId("input-important");
